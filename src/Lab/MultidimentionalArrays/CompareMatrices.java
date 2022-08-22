24276697ee002432e823e0a696b3017e73fb9e97
YanaMarkova <yana_markova@yahoo.com>
2022-08-22 16:48:09 +0200

    Initial commit

---
+++ src/Lab/MultidimentionalArrays/CompareMatrices.java
@@ -0,0 +1,62 @@
+package Lab.MultidimentionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CompareMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] firstMatrix = readMatrix(scanner);
+        int[][] secondMatrix = readMatrix(scanner);
+
+        boolean areEqual = compareMatrices(firstMatrix, secondMatrix);
+
+        String output = areEqual ? "equal" : "not equal";
+
+        System.out.println(output);
+    }
+
+    private static boolean compareMatrices(int[][] firstMatrix, int[][] secondMatrix) {
+
+        if (firstMatrix.length != secondMatrix.length) {
+            return false;
+        }
+
+        for (int row = 0; row < firstMatrix.length; row++) {
+            int[] firstArr = firstMatrix[row];
+            int[] secondArr = secondMatrix[row];
+
+            if (firstArr.length != secondArr.length) {
+                return false;
+            }
+
+            for (int col = 0; col < firstArr.length; col++) {
+                int firstElement = firstArr[col];
+                int secondElement = secondArr[col];
+
+                if (firstElement != secondElement) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int[] dimensions = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int rows = dimensions[0];
+        int columns = dimensions[1];
+
+        int[][] matrix = new int[rows][columns];
+
+        for (int row = 0; row < rows; row++) {
+            int[] columnArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            matrix[row] = columnArr;
+        }
+        return matrix;
+    }
+}
