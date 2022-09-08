b3784be9cf3f50991547ff9816701efbca297012
YanaMarkova <yana_markova@yahoo.com>
2022-09-08 12:23:27 +0200

    Initial commit

---
+++ src/Exercise/MultidimentionalArrays/MatrixOfPalindromes.java
@@ -0,0 +1,39 @@
+package Exercise.MultidimentionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatrixOfPalindromes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] dimensions = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        String[][] matrix = new String[rows][cols];
+
+        fillMatrix(rows, cols, matrix);
+        printMatrix(rows, cols, matrix);
+    }
+
+    private static void fillMatrix(int rows, int cols, String[][] matrix) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                char outside = (char) ('a' + row);
+                char middle = (char) (outside + col);
+                matrix[row][col] = String.valueOf(outside) + middle + outside;
+            }
+        }
+    }
+
+    private static void printMatrix(int rows, int cols, String[][] matrix) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
