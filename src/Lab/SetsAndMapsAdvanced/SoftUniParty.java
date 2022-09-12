0eda3252e1db096777c577508d3c9620d70e1ba9
YanaMarkova <yana_markova@yahoo.com>
2022-09-12 13:37:44 +0200

    Initial commit

---
+++ src/Lab/SetsAndMapsAdvanced/SoftUniParty.java
@@ -0,0 +1,43 @@
+package Lab.SetsAndMapsAdvanced;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Set<String> vipList = new TreeSet<>();
+        Set<String> regularList = new TreeSet<>();
+
+        while (!input.equals("PARTY")) {
+
+            if (Character.isDigit(input.charAt(0))) {
+                vipList.add(input);
+            } else {
+                regularList.add(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        while (!input.equals("END")) {
+
+            if (Character.isDigit(input.charAt(0))) {
+                vipList.remove(input);
+            } else {
+                regularList.remove(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        int guestsMissed = vipList.size() + regularList.size();
+        System.out.println(guestsMissed);
+        vipList.forEach(System.out::println);
+        regularList.forEach(System.out::println);
+    }
+}
