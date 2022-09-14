2c49cd2274c55888a4620e59d1325a6e0babff4e
YanaMarkova <yana_markova@yahoo.com>
2022-09-14 17:09:53 +0200

    Initial commit

---
+++ src/Lab/SetsAndMapsAdvanced/LargestThreeNumbers.java
@@ -0,0 +1,15 @@
+package Lab.SetsAndMapsAdvanced;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class LargestThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).sorted(Comparator.reverseOrder()).limit(3).map(String::valueOf).collect(Collectors.joining(" ")));
+
+    }
+}
