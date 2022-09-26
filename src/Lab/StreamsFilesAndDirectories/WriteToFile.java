43c975f0e0c43c89970de1bc65aa8d7e1ab3f718
YanaMarkova <yana_markova@yahoo.com>
2022-09-26 15:43:45 +0200

    Initial commit

--- src/Lab/StreamsFilesAndDirectories/WriteToFile.java
+++ src/Lab/StreamsFilesAndDirectories/WriteToFile.java
@@ -1,7 +1,6 @@
 package Lab.StreamsFilesAndDirectories;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Set;
