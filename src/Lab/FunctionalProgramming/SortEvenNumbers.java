eb131e6b72e1693eada75048355aa5962f2f51f3
YanaMarkova <yana_markova@yahoo.com>
2022-10-04 15:39:28 +0200

    Initial commit

--- src/Lab/FunctionalProgramming/SortEvenNumbers.java
+++ src/Lab/FunctionalProgramming/SortEvenNumbers.java
@@ -10,8 +10,11 @@ public class SortEvenNumbers {
 
         String[] numbers = scanner.nextLine().split(", ");
 
+        // Filtering the even elements and printing them
         List<String> evenNumbers = Arrays.stream(numbers).map(Integer::parseInt).filter(e -> e % 2 == 0).map(Object::toString).collect(Collectors.toList());
         System.out.println(String.join(", ", evenNumbers));
+
+        // Sorting the elements and printing them
         evenNumbers = evenNumbers.stream().map(Integer::parseInt).sorted(Integer::compareTo).map(Object::toString).collect(Collectors.toList());
         System.out.println(String.join(", ", evenNumbers));
     }
