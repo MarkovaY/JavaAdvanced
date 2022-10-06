5cceeb34880f32d9530116a2ca2d94df83509ac3
YanaMarkova <yana_markova@yahoo.com>
2022-10-06 12:07:31 +0200

    Initial commit

---
+++ src/Lab/FunctionalProgramming/SumNumbersBiFunction.java
@@ -0,0 +1,24 @@
+package Lab.FunctionalProgramming;
+
+import java.util.Scanner;
+import java.util.function.BiFunction;
+
+public class SumNumbersBiFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(", ");
+
+        System.out.println("Count = " + input.length);
+
+        int sum = 0;
+
+        BiFunction<Integer, String, Integer> parseAndSum = (x, y) -> x + Integer.parseInt(y);
+
+        for (int i = 0; i < input.length; i++) {
+            sum = parseAndSum.apply(sum, input[i]);
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
