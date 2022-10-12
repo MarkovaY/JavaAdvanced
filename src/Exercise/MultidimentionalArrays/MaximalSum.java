5847338982683614df6cd2d598dd6826eed546d2
YanaMarkova <yana_markova@yahoo.com>
2022-10-12 15:30:54 +0200

    Initial commit

---
+++ src/Exercise/MultidimentionalArrays/MaximalSum.java
@@ -0,0 +1,59 @@
+package Exercise.MultidimentionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaximalSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] dimensions = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            int[] columnArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            matrix[row] = columnArr;
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        int bestStartingRow = 0;
+        int bestStartingCol = 0;
+
+        //Finding every 3 X 3 matrix in the main matrix
+        for (int row = 0; row < rows - 2; row++) {
+            for (int col = 0; col < cols - 2; col++) {
+                // First, longer solution to find the sum of every 3 X 3 matrix
+//                int sum = matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2]
+//                        + matrix[row + 1][col] + matrix[row + 1][col + 1] + matrix[row + 1][col + 2]
+//                        + matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2];
+
+                // Finding the sum in the 3 X 3 matrices in the main matrix
+                int sum = 0;
+                for (int currentRow = row; currentRow < row + 3; currentRow++) {
+                    for (int currentCol = col; currentCol < col + 3; currentCol++) {
+                        sum += matrix[currentRow][currentCol];
+                    }
+                }
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    bestStartingRow = row;
+                    bestStartingCol = col;
+                }
+            }
+        }
+        System.out.println("Sum = " + maxSum);
+        //Printing the best 3 X 3 matrix
+        for (int row = bestStartingRow; row < bestStartingRow + 3; row++) {
+            for (int col = bestStartingCol; col < bestStartingCol + 3; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
