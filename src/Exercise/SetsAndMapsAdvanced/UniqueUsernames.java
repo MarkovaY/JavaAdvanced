513616d348eeecd830d11f1773598e3510634b16
YanaMarkova <yana_markova@yahoo.com>
2022-10-14 14:18:18 +0200

    Initial commit

--- src/Exercise/SetsAndMapsAdvanced/UniqueUsernames.java
+++ src/Exercise/SetsAndMapsAdvanced/UniqueUsernames.java
@@ -7,16 +7,20 @@ public class UniqueUsernames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Reading how many entries we'll get
         int n = Integer.parseInt(scanner.nextLine());
 
         LinkedHashSet<String> uniqueNames = new LinkedHashSet<>();
 
         for (int i = 0; i < n; i++) {
+            // Reading names
             String name = scanner.nextLine();
 
+            //Adding names to the set
             uniqueNames.add(name);
         }
 
+        // Printing all the entries of the set
         uniqueNames.forEach(System.out::println);
     }
 }
