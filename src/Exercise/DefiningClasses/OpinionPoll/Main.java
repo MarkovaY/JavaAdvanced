a3532ced8b0716a43982f53c3ea995195d0c7bb8
YanaMarkova <yana_markova@yahoo.com>
2022-10-20 15:18:54 +0200

    Initial commit

--- src/Exercise/DefiningClasses/OpinionPoll/Main.java
+++ src/Exercise/DefiningClasses/OpinionPoll/Main.java
@@ -9,22 +9,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Reading how many people we will be given
         int countPeople = Integer.parseInt(scanner.nextLine());
 
         List<Person> people = new ArrayList<>();
         for (int i = 0; i < countPeople; i++) {
-
+            // Reading the data about each person
             String[] data = scanner.nextLine().split("\\s+");
 
             String name = data[0];
             int age = Integer.parseInt(data[1]);
 
+            // Filtering people by age
             if (age > 30) {
-
                 Person person = new Person(name, age);
                 people.add(person);
             }
         }
-        people.stream().filter(person -> person.getAge() > 30).sorted((left, right) -> left.getName().compareTo(right.getName())).forEach(person -> System.out.println(person.getName() + " - " + person.getAge()));
+        //Sorting people in ascending order and printing the results
+        people.stream().sorted((left, right) -> left.getName().compareTo(right.getName())).forEach(person -> System.out.println(person.getName() + " - " + person.getAge()));
     }
 }
