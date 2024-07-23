c5429204e3f06d47d448d17a83bce280fcea7a26
YanaMarkova <yana_markova@yahoo.com>
2024-07-23 16:02:45 +0200

    Initial commit

---
+++ src/Lab/Generics/JarOfT/Main.java
@@ -0,0 +1,15 @@
+package Lab.Generics.JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<Integer> integers = new Jar<>();
+
+        integers.add(73);
+        integers.add(15);
+
+        integers.remove();
+
+
+    }
+}
