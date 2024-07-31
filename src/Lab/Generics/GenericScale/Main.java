970ac170dc2088c5db6a462114b36cdc6610f66d
YanaMarkova <yana_markova@yahoo.com>
2024-07-31 16:49:18 +0200

    Initial commit

--- src/Lab/Generics/GenericScale/Main.java
+++ src/Lab/Generics/GenericScale/Main.java
@@ -1,8 +1,18 @@
 package Lab.Generics.GenericScale;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        int first = Integer.parseInt(scanner.nextLine());
+
+        int second = Integer.parseInt(scanner.nextLine());
+
+        Scale<Integer> stringScale = new Scale<>(first, second);
+
+        System.out.println(stringScale.getHeavier());
     }
 }
