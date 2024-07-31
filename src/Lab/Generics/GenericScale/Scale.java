624260adad362e87ebd164eeb9160bb646eb7ea7
YanaMarkova <yana_markova@yahoo.com>
2024-07-31 16:58:44 +0200

    Initial commit

--- src/Lab/Generics/GenericScale/Scale.java
+++ src/Lab/Generics/GenericScale/Scale.java
@@ -1,12 +1,12 @@
 package Lab.Generics.GenericScale;
 
-public class Scale<T> {
+public class Scale<T extends Comparable<T>> {
 
-//    Create a class Scale<T> that holds two elements - left and right. The scale should receive the elements through its single constructor:
-//            •	Scale(T left, T right)
-//    The scale should have a single method:
-//            •	T getHeavier()
-//    The greater of the two elements is heavier. The method should return null if elements are equal.
+        //    Create a class Scale<T> that holds two elements - left and right. The scale should receive the elements through its single constructor:
+        //            •	Scale(T left, T right)
+        //    The scale should have a single method:
+        //            •	T getHeavier()
+        //    The greater of the two elements is heavier. The method should return null if elements are equal.
 
     private T left;
     private T right;
@@ -18,9 +18,16 @@ public class Scale<T> {
 
     public T getHeavier(){
 
-        if(left == right){
+        if(left.equals(right)){
             return null;
         }
-        return left;
+
+        int result = left.compareTo(right);
+
+        if(result > 0) {
+            return left;
+        }
+
+        return right;
     }
 }
