8a9b2d7b4f7793b62ffe2cd205cbf0eff362d6f2
YanaMarkova <yana_markova@yahoo.com>
2024-08-09 16:18:27 +0200

    Initial commit

--- src/Lab/IteratorsAndComparators/Book/Book.java
+++ src/Lab/IteratorsAndComparators/Book/Book.java
@@ -1,4 +1,20 @@
 package Lab.IteratorsAndComparators.Book;
 
 public class Book {
+
+        //    Create a class Book from the UML diagram below:
+        //    Book
+        //-	title: String
+        //-	year: int
+        //-	authors: List<String>
+        //-	setTitle(String)
+        //-	setYear(String)
+        //-	setAuthors(String…)
+        //+	getTitle(): String
+        //+	getYear(): int
+        //+	getAuthors(): List<String>
+        //    You can use only one constructor. There can be no authors, one author, or many authors.
+
+
+
 }
