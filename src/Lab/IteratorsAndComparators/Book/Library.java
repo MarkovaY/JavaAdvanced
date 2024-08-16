0dedc51f742239f6f8c17bc22ae967c25afca4c6
YanaMarkova <yana_markova@yahoo.com>
2024-08-16 16:09:28 +0200

    Initial commit

--- src/Lab/IteratorsAndComparators/Book/Library.java
+++ src/Lab/IteratorsAndComparators/Book/Library.java
@@ -1,15 +1,44 @@
 package Lab.IteratorsAndComparators.Book;
 
-public class Library {
+import java.util.Iterator;
 
-//    Create a class Library from the UML diagram below:
-//    <<Iterable<Book>>> Library
-//      -	books: Book[]
-//      +	iterator(): Iterator<Book>
-//    Create a nested class LibIterator from the UML diagram below:
-//            <<Iterator<Book>>> LibIterator
-//      -	counter: int
-//      +	hasNext(): boolean
-//      +	next(): Book
+public class Library implements Iterable<Book> {
+
+    //    Create a class Library from the UML diagram below:
+    //    <<Iterable<Book>>> Library
+    //      -	books: Book[]
+    //      +	iterator(): Iterator<Book>
+    //    Create a nested class LibIterator from the UML diagram below:
+    //            <<Iterator<Book>>> LibIterator
+    //      -	counter: int
+    //      +	hasNext(): boolean
+    //      +	next(): Book
+
+    private class LibIterator implements Iterator<Book> {
+
+        int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[index++];
+        }
+    }
+
+    private Book[] books;
+
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibIterator();
+    }
 
 }
