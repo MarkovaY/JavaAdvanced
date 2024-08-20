c2a7faa6895324eed101b46b47ae2f4b4dc32cfd
YanaMarkova <yana_markova@yahoo.com>
2024-08-20 15:45:51 +0200

    Initial commit

--- src/Lab/DefiningClasses/BankAccount/BankAccount.java
+++ src/Lab/DefiningClasses/BankAccount/BankAccount.java
@@ -2,6 +2,22 @@ package Lab.DefiningClasses.BankAccount;
 
 public class BankAccount {
 
+//    Create class BankAccount.
+//    The class should have private fields for:
+//            •	Id: int (Starts from 1 and increments for every new account)
+//            •	Balance: double
+//            •	Interest rate: double (Shared for all accounts. Default value: 0.02)
+//    The class should also have public methods for:
+//            •	setInterestRate(double interest): void (static)
+//            •	getInterest(int Years): double
+//            •	deposit(double amount): void
+//    Create a test client supporting the following commands:
+//            •	Create
+//            •	Deposit {Id} {Amount}
+//            •	SetInterest {Interest}
+//            •	GetInterest {ID} {Years}
+//            •	End
+
     private static int nextId = 1;
     private int id;
     private double balance;
