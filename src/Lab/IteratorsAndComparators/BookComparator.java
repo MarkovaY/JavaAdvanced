c9d3bcd95791c1f84120dd9d424c7bb14065e186
YanaMarkova <yana_markova@yahoo.com>
2024-08-21 15:12:07 +0200

    Initial commit

---
+++ src/Lab/IteratorsAndComparators/BookComparator.java
@@ -0,0 +1,13 @@
+package Lab.IteratorsAndComparators;
+
+public class BookComparator {
+
+//    Create a class BookComparator from the UML diagram below:
+//         <<Comparator<Book>>> BookComparator
+//            +	compare(Book, Book): int
+//    BookComparator has to compare two books by:
+//            1.	Book title.
+//            2.	Year of publishing a book.
+
+
+}
